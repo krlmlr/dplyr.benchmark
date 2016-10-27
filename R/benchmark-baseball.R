a558e26406ca800c171e31c2d40ec3d6810b093b
Kirill Müller <krlmlr+r@mailbox.org>
2016-10-27 15:33:22 +0200

    remove times argument

--- R/benchmark-baseball.R
+++ R/benchmark-baseball.R
@@ -33,7 +33,6 @@ hall_of_fame_dt <- tbl_dt(HallOfFame) %>% filter(inducted == "Y") %>%
     dplyr_dt = batting_dt %>% group_by(playerID) %>% summarise(ab = mean(AB)),
     dt_raw =   batting_dt[, list(ab = mean(AB)), by = playerID],
     base =     tapply(batting_df$AB, batting_df$playerID, FUN = mean)
-    times = 5
   )
 
   ## ----sumarise-mean_------------------------------------------------------
@@ -41,8 +40,7 @@ hall_of_fame_dt <- tbl_dt(HallOfFame) %>% filter(inducted == "Y") %>%
     dplyr_df = batting_df %>% group_by(playerID) %>% summarise(ab = mean_(AB)),
     dplyr_dt = batting_dt %>% group_by(playerID) %>% summarise(ab = mean_(AB)),
     dt_raw =   batting_dt[, list(ab = mean_(AB)), by = playerID],
-    base =     tapply(batting_df$AB, batting_df$playerID, FUN = mean_),
-    times = 5
+    base =     tapply(batting_df$AB, batting_df$playerID, FUN = mean_)
   )
 
   ## ----arrange-------------------------------------------------------------
@@ -50,8 +48,7 @@ hall_of_fame_dt <- tbl_dt(HallOfFame) %>% filter(inducted == "Y") %>%
     dplyr_df = batting_df %>% arrange(playerID, yearID),
     dplyr_dt = batting_dt %>% arrange(playerID, yearID),
     dt_raw =   setkey(copy(batting_dt), playerID, yearID),
-    base   =   batting_dt[order(batting_df$playerID, batting_df$yearID), ],
-    times = 2
+    base   =   batting_dt[order(batting_df$playerID, batting_df$yearID), ]
   )
 
   ## ----filter--------------------------------------------------------------
@@ -60,16 +57,14 @@ hall_of_fame_dt <- tbl_dt(HallOfFame) %>% filter(inducted == "Y") %>%
     dplyr_dt = batting_dt %>% group_by(playerID) %>% filter(G == max(G)),
     dt_raw   = batting_dt[batting_dt[, .I[G == max(G)], by = playerID]$V1],
     base   =   batting_df[ave(batting_df$G, batting_df$playerID, FUN = max) ==
-      batting_df$G, ],
-    times = 2
+      batting_df$G, ]
   )
 
   ## ----mutate--------------------------------------------------------------
   mutate = lazyeval::dots_capture(
     dplyr_df  = batting_df %>% group_by(playerID) %>% mutate(r = rank(desc(AB))),
     dplyr_dt  = batting_dt %>% group_by(playerID) %>% mutate(r = rank(desc(AB))),
-    dt_raw =    copy(batting_dt)[, rank := rank(desc(AB)), by = playerID],
-    times = 2
+    dt_raw =    copy(batting_dt)[, rank := rank(desc(AB)), by = playerID]
   )
 
   ## ----mutate2-------------------------------------------------------------
@@ -79,8 +74,7 @@ hall_of_fame_dt <- tbl_dt(HallOfFame) %>% filter(inducted == "Y") %>%
     dplyr_dt = batting_dt %>% group_by(playerID) %>%
       mutate(cyear = yearID - min(yearID) + 1),
     dt_raw =   copy(batting_dt)[, cyear := yearID - min(yearID) + 1,
-      by = playerID],
-    times = 5
+      by = playerID]
   )
 
   ## ----mutate_hybrid-------------------------------------------------------
@@ -89,35 +83,30 @@ hall_of_fame_dt <- tbl_dt(HallOfFame) %>% filter(inducted == "Y") %>%
   )
 
   windowed_reg = lazyeval::dots_capture(
-    regular  = batting_df %>% group_by(playerID) %>% mutate(r = min_rank_(AB)),
-    times = 2
+    regular  = batting_df %>% group_by(playerID) %>% mutate(r = min_rank_(AB))
   )
 
   ## ------------------------------------------------------------------------
   left_join = lazyeval::dots_capture(
     dplyr_df = left_join(master_df, hall_of_fame_df, by = "playerID"),
     dplyr_dt = left_join(master_dt, hall_of_fame_dt, by = "playerID"),
-    base     = merge(master_df, hall_of_fame_df, by = "playerID", all.x = TRUE),
-    times = 10
+    base     = merge(master_df, hall_of_fame_df, by = "playerID", all.x = TRUE)
   )
 
   inner_join = lazyeval::dots_capture(
     dplyr_df = inner_join(master_df, hall_of_fame_df, by = "playerID"),
     dplyr_dt = inner_join(master_dt, hall_of_fame_dt, by = "playerID"),
-    base     = merge(master_df, hall_of_fame_df, by = "playerID"),
-    times = 10
+    base     = merge(master_df, hall_of_fame_df, by = "playerID")
   )
 
   semi_join = lazyeval::dots_capture(
     dplyr_df = semi_join(master_df, hall_of_fame_df, by = "playerID"),
-    dplyr_dt = semi_join(master_dt, hall_of_fame_dt, by = "playerID"),
-    times = 10
+    dplyr_dt = semi_join(master_dt, hall_of_fame_dt, by = "playerID")
   )
 
   anti_join = lazyeval::dots_capture(
     dplyr_df = anti_join(master_df, hall_of_fame_df, by = "playerID"),
-    dplyr_dt = anti_join(master_dt, hall_of_fame_dt, by = "playerID"),
-    times = 10
+    dplyr_dt = anti_join(master_dt, hall_of_fame_dt, by = "playerID")
   )
 
 }
