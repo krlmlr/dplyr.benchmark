6f9c255e79e7e0d12c6d4bd5e27fc278ac31bd63
Kirill Müller <krlmlr+r@mailbox.org>
2016-10-29 23:09:42 +0200

    write CSV

--- R/microbenchmark.R
+++ R/microbenchmark.R
@@ -1,18 +1,15 @@
-run_microbenchmark <- function(ref = "master", repo = dplyr_repo()) {
+benchmark <- function(ref = "master", repo = dplyr_repo()) {
   repo_dir <- dplyr_clone(ref, repo = repo)
 
-  mb <- withr::with_dir(repo_dir, do_run_microbenchmark())
+  mb_tidy <- run_microbenchmark(repo_dir)
 
-  mb_tidy <-
-    mb %>%
-    tibble::enframe() %>%
-    mutate_(name = ~forcats::fct_inorder(name)) %>%
-    tidyr::unnest() %>%
-    select(-expr) %>%
-    group_by_(~name) %>%
-    summarize_(median_time = ~median(time)) %>%
-    ungroup %>%
-    mutate_(calibrated_time = ~median_time / median_time[[1]])
+  sha <- withr::with_dir(repo_dir, system2("git", c("rev-parse", ref), stdout = TRUE))
+  write_microbenchmark(mb_tidy, sha)
+}
+
+run_microbenchmark <- function(repo_dir) {
+  mb <- withr::with_dir(repo_dir, do_run_microbenchmark())
+  mb_tidy <- tidy_microbenchmark(mb)
 
   mb_tidy
 }
@@ -31,12 +28,18 @@ do_run_microbenchmark <- function() {
   )
 }
 
-do_stuff <- ~{
-  r <- git2r::repository()
-  sha <- devtools%:::%git_repo_sha1(r)
+tidy_microbenchmark <- function(mb) {
+  mb %>%
+    tibble::enframe() %>%
+    mutate_(name = ~forcats::fct_inorder(name)) %>%
+    tidyr::unnest() %>%
+    select(-expr) %>%
+    group_by_(~name) %>%
+    summarize_(median_time = ~median(time)) %>%
+    ungroup %>%
+    mutate_(calibrated_time = ~median_time / median_time[[1]])
+}
 
-  write.csv(mb_tidy, file.path("benchmark", paste0(sha, ".csv")))
-  cat("Done: ", sha, "\n", sep = "")
-
-  print(sessionInfo())
+write_microbenchmark <- function(mb_tidy, sha) {
+  write.csv(mb_tidy, file.path("inst", "benchmark", paste0(sha, ".csv")))
 }
