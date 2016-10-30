2508d26c6ae071dd196d8757a509b40a94b672fd
Kirill Müller <krlmlr+r@mailbox.org>
2016-10-30 15:08:29 +0100

    detect jumps with tsoutlier

--- R/plot.R
+++ R/plot.R
@@ -10,7 +10,7 @@ get_plot_data <- function(ref = "master") {
   plot_data <- log_df %>%
     inner_join(full_data, by = "sha")
 
-  plot_data
+  detect_jumps(plot_data)
 }
 
 get_full_data <- function() {
@@ -20,5 +20,27 @@ get_full_data <- function() {
                            as.character(csv_files))
   csv_data <- lapply(csv_files, read.csv, row.names = NULL, stringsAsFactors = FALSE)
   full_data <- bind_rows(csv_data, .id = "sha")
-  full_data
+  tbl_df(full_data)
+}
+
+detect_jumps <- function(plot_data) {
+  plot_data %>%
+    tidyr::nest(-name) %>%
+    mutate_(data = ~lapply(data, detect_jumps_one)) %>%
+    tidyr::unnest()
+}
+
+detect_jumps_one <- function(data) {
+  data_ts <- ts(data$calibrated_time)
+
+  fit <- arima(data_ts, order = c(0L, 1L, 0L))
+  resid <- residuals(fit)
+  pars <- tsoutliers::coefs2poly(fit)
+
+  outliers <- tsoutliers::locate.outliers(resid, pars, types = "LS")
+
+  data %>%
+    mutate_(ind = ~row_number()) %>%
+    left_join(outliers, by = "ind") %>%
+    select(-ind)
 }
